31e5b3c3b6f35fc84524281732e053b182029348
SimpYena <jointhe611@gmail.com>
2024-10-18 10:27:37 +0700

    Handle refresh token

--- src/api/modules/users/users.controller.ts
+++ src/api/modules/users/users.controller.ts
@@ -1,9 +1,10 @@
-import { Controller, Post, Body, UsePipes, ValidationPipe, HttpCode, HttpStatus } from '@nestjs/common';
+import { Controller, Post, Body, UsePipes, ValidationPipe, HttpCode, HttpStatus, UseGuards, Request } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { plainToInstance } from 'class-transformer';
 import { LoginDTO } from './dto/login.dto';
 import { TokenDTO } from './dto/token.dto';
+import { RefreshTokenJwtGuard } from './guards/refresh-token-auth.guard';
 
 @Controller('auth')
 export class UsersController {
@@ -20,5 +21,14 @@ export class UsersController {
   @HttpCode(HttpStatus.OK)
   async login(@Body() loginDTO: LoginDTO): Promise<TokenDTO> {
     return plainToInstance(TokenDTO, await this.usersService.login(loginDTO));
+  }
+
+  @Post('refresh-token')
+  @UseGuards(RefreshTokenJwtGuard)
+  async refreshToken(@Request() req): Promise<TokenDTO> {
+    return plainToInstance(
+      TokenDTO,
+      await this.usersService.handleRefreshToken(req.user),
+    );
   }
 }
--- src/api/modules/users/users.module.ts
+++ src/api/modules/users/users.module.ts
@@ -4,13 +4,20 @@ import { UsersController } from './users.controller';
 import { User } from 'src/api/common/entities/user.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { JwtModule } from '@nestjs/jwt';
+import { AccessToken } from 'src/api/common/entities/access-token.entity';
+import { RefreshToken } from 'src/api/common/entities/refresh-token.entity';
+import { JwtStrategy } from '../strategies/jwt-strategy';
+import { RefreshTokenJwtStrategy } from '../strategies/refresh-token-jwt.strategy';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User]),
+  imports: [TypeOrmModule.forFeature([User, AccessToken, RefreshToken]),
   JwtModule.register({})
 ],
   exports: [UsersService],
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [UsersService,
+    JwtStrategy,
+    RefreshTokenJwtStrategy
+  ],
 })
 export class UsersModule {}
