6cbb2ec148eb3c97cb0940811f5599b53bcd98b3
SimpYena <jointhe611@gmail.com>
2024-12-04 10:51:10 +0700

    implement assessment test api

--- src/api/modules/AI/ai.controller.ts
+++ src/api/modules/AI/ai.controller.ts
@@ -1,20 +1,33 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
 import { TestOptionDTO } from './dto/test-option.dto';
 import { AiService } from './ai.service';
+import { JwtGuard } from '../users/guards/jwt-auth.guard';
+import { AwnswerDTO } from './dto/answer.dto';
 
-@Controller()
+@Controller('generate')
 export class AiController {
     constructor(
         private readonly aiService: AiService
     ){}
 
-    @Post('/generate/reading')
+    @Post('reading')
     async generateReadingSection(@Body() testOptionDTO: TestOptionDTO) {
         return this.aiService.generateReadingSection(testOptionDTO);
     }
     
-    @Post('/generate/listening')
+    @Post('listening')
     async generateListeningSection(@Body() testOptionDTO: TestOptionDTO){
         return this.aiService.generateListeningSection(testOptionDTO);
+    }
+    @UseGuards(JwtGuard)
+    @Get('assessment')
+    async generateAssessmentTest(@Req() req) {
+        return this.aiService.generateAssessmentTest(req.user);
+    }
+    
+    @UseGuards(JwtGuard)
+    @Post('submit')
+    async submitAssessment(@Req() req, @Body() answerDTO: AwnswerDTO) {
+        return this.aiService.submitAssessment(req.user, answerDTO);
     }
 }
